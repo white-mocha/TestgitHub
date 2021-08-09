ed8da90a00ba336635560c08361f3357bcf3b887
JC-Choo <jicheolchoo@gmail.com>
2021-08-09 10:48:28 +0900

    Add toast extension

--- app/src/main/java/dev/chu/testapp/util/Extensions.kt
+++ app/src/main/java/dev/chu/testapp/util/Extensions.kt
@@ -1,3 +1,9 @@
 package dev.chu.testapp.util
 
+import android.content.Context
+import android.widget.Toast
+
 val Any.TAG get() = this::class.java.simpleName ?: this.toString()
+
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) =
+    Toast.makeText(this, message, duration).show()
