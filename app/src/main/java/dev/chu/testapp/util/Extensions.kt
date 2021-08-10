fbbeae7c32ee980ce22a8c1452185c034c31f422
JC-Choo <iwhimo@gmail.com>
2021-08-10 14:06:02 +0900

    Add extensions
    Signed-off-by: JC-Choo <iwhimo@gmail.com>

--- app/src/main/java/dev/chu/testapp/util/Extensions.kt
+++ app/src/main/java/dev/chu/testapp/util/Extensions.kt
@@ -3,6 +3,7 @@ package dev.chu.testapp.util
 import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
 
 val Any.TAG get() = this::class.java.simpleName ?: this.toString()
 
@@ -11,3 +12,9 @@ fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) =
 
 fun Context.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) =
     Toast.makeText(this, resId, duration).show()
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_SHORT) =
+    Toast.makeText(requireContext(), message, duration).show()
+
+fun Fragment.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) =
+    Toast.makeText(requireContext(), resId, duration).show()
